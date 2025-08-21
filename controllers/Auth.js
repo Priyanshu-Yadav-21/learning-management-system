f920c516e8723c1b3f71aaa911a3ef1e0839d451
Priyanshu-Yadav-21 <py21092002@gmail.com>
2025-08-22 02:30:16 +0530

    feat: add Authentication controller

---
+++ controllers/Auth.js
@@ -0,0 +1,55 @@
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator");
+
+exports.sendOTP = async (req, res) => {
+
+    try {
+        const { email } = req.body;
+
+        const checkUserPresent = await User.findOne({ email });
+
+        if (checkUserPresent) {
+            return res.status(401).json({
+                success: false,
+                message: "User Already Exist"
+            })
+        }
+
+        var otp = otpGenerator.generate(6, {
+            upperCaseAlphabets: false,
+            lowerCaseAlphabets: false,
+            specialChars: false
+        });
+
+        const findOTP = await OTP.findOne({ otp: otp });
+
+        while (findOTP) {
+            var otp = otpGenerator.generate(6, {
+                upperCaseAlphabets: false,
+                lowerCaseAlphabets: false,
+                specialChars: false
+            })
+
+            const findOTP = await OTP.findOne({ otp: otp });
+        }
+
+        const otpPayload = {email, otp};
+
+        const otpBody = await OTP.create(otpPayload);
+        console.log(otpBody);
+
+        res.status(200).json({
+            success: true,
+            message: "OTP sent successfully",
+            otp,
+        })
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+};
