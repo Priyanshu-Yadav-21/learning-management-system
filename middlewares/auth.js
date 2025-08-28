b10047e5eb15bb3e28a36376f9fcacbaf2c91114
Priyanshu-Yadav-21 <py21092002@gmail.com>
2025-08-28 23:53:13 +0530

    feat: add functions for different roles in middlewares

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-exports.auth = async (req, res) => {
+exports.auth = async (req, res, next) => {
     try{
         const token = req.cookies.token || req.body.token || req.header("Authorisation").replace("Bearer", "");
 
@@ -15,7 +15,7 @@ exports.auth = async (req, res) => {
 
         //verify the token
         try {
-            const decode = await jwt.verify(token, process.env.JWT_SECRET);
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
             console.log(decode);
             req.user = decode;
         }
@@ -25,8 +25,71 @@ exports.auth = async (req, res) => {
                 message:"token is invalid"
             });
         }
+
+        next();
     }
     catch(error) {
-
+        return res.success(401).json({
+            success:false,
+            message:"error while validating the token"
+        });
     }
 }
+
+//isStudent
+exports.isStudent = async (req, res, nect) => {
+    try {
+        if(req.user.accountType !== "Student") {
+            return res.status(401).json({
+                success: false,
+                message: "This is a protected route for student"
+            });
+        }
+        next();
+    }
+    catch(error) {
+        return res.status(500).json({
+            success: false,
+            message: "User cannot be varified, please try again"
+        });
+    }
+}
+
+//isInstructor
+exports.isInstructor = async (req, res, nect) => {
+    try {
+        if(req.user.accountType !== "Instructor") {
+            return res.status(401).json({
+                success: false,
+                message: "This is a protected route for Instructor"
+            });
+        }
+        next();
+    }
+    catch(error) {
+        return res.status(500).json({
+            success: false,
+            message: "User cannot be varified, please try again"
+        });
+    }
+}
+
+//isAdmin
+exports.isAdmin = async (req, res, nect) => {
+    try {
+        if(req.user.accountType !== "Admin") {
+            return res.status(401).json({
+                success: false,
+                message: "This is a protected route for Admin"
+            });
+        }
+        next();
+    }
+    catch(error) {
+        return res.status(500).json({
+            success: false,
+            message: "User cannot be varified, please try again"
+        });
+    }
+}
+
